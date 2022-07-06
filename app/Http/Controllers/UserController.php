e4128c2c6b4ae2d91993e04119b820968c1270bc
Ngo Nhat Tri <nhattri1503@gmail.com>
2022-07-06 17:58:12 +0900

    Complete Change Password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,4 +50,24 @@ class UserController extends Controller
             'password' => 'Wrong username or password',
         ]);
     }
+
+    public function password() {
+        $data['title'] = 'Change Password';
+        return view('user/password', $data);
+    }
+
+    public function password_action(Request $request) {
+        $request->validate([
+            'old_password' => 'required|current_password',
+            'new_password' => 'required|confirmed',
+        ]);
+        $user = User::find(Auth::id());
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+        $request->session()->regenerate();
+        
+        return back()->with(
+            'success', 'Password Changed!',
+        );
+    }
 }
