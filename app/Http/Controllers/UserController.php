2dd7cdee6d86e5b8ff40d6d37f86403c3595a44d
Ngo Nhat Tri <nhattri1503@gmail.com>
2022-07-06 17:59:30 +0900

    Complete Logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,4 +70,11 @@ class UserController extends Controller
             'success', 'Password Changed!',
         );
     }
+    
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
